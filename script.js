c8bd7eae1f20fb171ad1aa0f2be2f30d75827781
juds-lee <judyjungmin.lee@gmail.com>
2022-03-26 17:42:52 -0400

    added JS for functionality. Nav menu, comment section, image carousel

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //   nav menu functionality
-
   document.querySelector('.menu-btn').addEventListener("click", () => {
-    
               document.querySelector(".nav-menu").classList.toggle("show");
               console.log(document.querySelector(".nav-menu"));
         })
-// adding comments
+
+// adding comments functionality
+
 // add event listener for form being submitted
 // save users input
 // dyanmically create a element for comment
@@ -15,7 +15,18 @@ const commentForm = document.querySelector("form");
 
  commentForm.addEventListener('submit', function(event) {
    event.preventDefault();
-   console.log("submit");
+  // get date
+  const date = new Date()
+
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "August", "October", "November", "December"];
+
+  let dateNumber = date.getDate();
+  let month = monthsOfYear[date.getMonth()];
+  let year = date.getFullYear();
+  let day = daysOfWeek[date.getDay()];
+  let todayIs = `${day} ${month} ${dateNumber}, ${year}`;
+  console.log(todayIs);
   // variable to store input element
   const commentElement = document.querySelector("textarea");
   const nameElement = document.querySelector("input");
@@ -37,14 +48,13 @@ const commentForm = document.querySelector("form");
     const paragraph = document.createElement("p")
      paragraph.textContent = comment
     const nameForm = document.createElement("p")
-      nameForm.textContent = name + " comments..."
+      nameForm.textContent = todayIs + " By " + name 
 
     //  create an img to hold user img
     pic = document.createElement("img");
     pic.src= "./assets/comment-2.jpg"
 
     
-
     // append the p to the comment area as a child
     nameArea.appendChild(nameForm)
     nameArea.appendChild(paragraph)
@@ -58,3 +68,51 @@ const commentForm = document.querySelector("form");
     alert("Please put in a comment! :)")
   }
 })
+
+// Image Carousel 
+const nextButton = document.querySelector(".right-arrow");
+const prevButton = document.querySelector(".left-arrow");
+const track = document.querySelector(".img-car")
+const slides = Array.from(track.children);
+const circleNav = document.querySelector(".carousel-tracker");
+const circle = Array.from(circleNav.children);
+
+// add event listener for the arrows on click             
+// move images right when i click arrow
+//  change class name of Icon when active and inactive
+nextButton.addEventListener("click", e => {
+  const currentSlide = track.querySelector(".active");
+  const currentCircle = circleNav.querySelector(".fas");
+  currentSlide.classList.remove("active");
+  currentCircle.classList.remove("fas");
+  currentCircle.classList.add("far");
+
+  let nextSlide = currentSlide.nextElementSibling;
+  let nextCircle = currentCircle.nextElementSibling;
+  if (!nextSlide && !nextCircle){
+    nextSlide = slides[0];
+    nextCircle = circle[0];
+  }  nextSlide.classList.add("active");
+    nextCircle.classList.add("fas");
+    nextCircle.classList.remove("far");
+    console.log(nextSlide, nextCircle);
+})
+
+prevButton.addEventListener("click", e => {
+  const currentSlide = track.querySelector(".active");
+  const currentCircle = circleNav.querySelector(".fas");
+  currentCircle.classList.remove("fas");
+  currentCircle.classList.add("far");
+  currentSlide.classList.remove("active");
+
+// move images left when i click arrow
+  let prevSlide = currentSlide.previousElementSibling;
+  let prevCircle = currentCircle.previousElementSibling;
+  if (!prevSlide && !prevCircle){
+    prevSlide = slides[slides.length-1];
+    prevCircle = circle[circle.length-1];
+  }  prevSlide.classList.add("active");
+    prevCircle.classList.add("fas");
+    prevCircle.classList.remove("far");
+})
+
